720ee491e3aed262ba388d5aa8c738bdc1616e21
avgfx <avgfx15@gmail.com>
2022-12-29 13:52:34 +0530

    E Commerce App MEAN Stack

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const config = require("./config/configSecret");
+
+const connectDB = require("./db/db");
+const sellerRoute = require("./routes/sellerRoutes");
+
+const userRoutes = require("./routes/userRoutes");
+const productRoute = require("./routes/productRoutes");
+
+const port = config.port;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRoutes);
+app.use("/seller", sellerRoute);
+app.use("/seller", productRoute);
+
+app.get("/", async (req, res) => {
+  res.json({ message: "Testing route" });
+});
+app.listen(port, () => {
+  console.log(`E - Commerce App is running on port ${port}`);
+});
